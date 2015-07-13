80cecb25632a6478e535a23167143186096d9fa6
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-13 15:04:35 -0400

    Added ?'s, some CSS, and basic js

---
+++ myscript.js
@@ -0,0 +1,55 @@
+var Franklin = null
+var Georgetown = null
+var CIA = null
+var darthVader = null
+var Federal_City = null
+var ra = null
+var charlie = null
+var bruce = null
+var halfMilly = null
+var third = null
+
+//target buttons
+//Question 1 
+//$('#franklin_R').on('click', function(){
+	//console.log("That's right!!"); //When user clicks A tell them they got the right answer
+//});
+$("franklin_R").click(function(){
+        $("p").slideUp();
+        });
+//Question 2 
+$('#georgeTown').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 3 
+$('#cia').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 4 
+$('#darthVader').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 5 
+$('#fedCity').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 6 
+$('#ra').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 7 
+$('#lEnfant').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 8 
+$('#bruce').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 9 
+$('#fiveHundred').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
+//Question 10 
+$('#third').on('click', function(){
+	alert("That's right!!"); //When user clicks A tell them they got the right answer
+});
